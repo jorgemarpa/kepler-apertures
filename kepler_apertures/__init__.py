205834e3c7c033aa224a4252ea3db1464178bc9f
Jorge <jorgemarpa@ug.uchile.cl>
2021-05-19 12:09:01 -0700

    updating classes

--- kepler_apertures/__init__.py
+++ kepler_apertures/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 import logging
-from .KeplerPSF import KeplerFFI, KeplerPSF
-from .ApertureMachine import EXBAsources
+from .KeplerPSF import KeplerPSF
+from .KeplerFFI import KeplerFFI
+from .ApertureMachine import EXBAMachine, EXBALightCurveCollection
 
 # Configure logging
 log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
 
 __version__ = "0.1.0"
-__all__ = ["KeplerFFI", "KeplerPSF", "EXBAsources"]
+__all__ = ["KeplerFFI", "KeplerPSF", "EXBAMachine", "EXBALightCurveCollection"]
